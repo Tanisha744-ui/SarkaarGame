4449c7a2160f69a561d744e7f73f2d0b4ef1a80d
Aayush Goel <aayushgoel260@gmail.com>
2026-01-08 15:30:20 +0530

    linking frontend with backend

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp",
         builder => builder
-            .WithOrigins("http://localhost:4200")
+            .WithOrigins("http://localhost:4200","https://sarkaarfrontend-7skg.onrender.com/")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
